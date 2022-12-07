c169d8480ccde8eeb1d997def7e94defed53416d
JacCreator <94701397+JacCreator@users.noreply.github.com>
2022-12-07 19:59:15 +0100

    Wszystko ok.

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
+import json
+import re
+import codecs
 
-if __name__ == '__main__':
+
+def main():
+    iter_link()
+
+
+def iter_link():
     base_url = 'https://sollux.pl/'
 
     product_links = []
+    f = codecs.open('result.json', 'w', 'utf8')
+    f.write('[')
+
     for x in range(0, 8):
         r = requests.get(f'https://sollux.pl/pl/menu/kinkiety-152?counter={x}')
         soup = BeautifulSoup(r.content, 'lxml')
@@ -13,10 +24,10 @@ if __name__ == '__main__':
                                        class_='product col-6 col-sm-4 pt-3 '
                                               'pb-md-3')
 
-        headers = {'UserAgent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
-                                'AppleWebKit/537.36 '
-                                '(KHTML, like Gecko) Chrome/72.0.3626.121 '
-                                'Safari/537.36'}
+        # headers = {'UserAgent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) '
+        #                         'AppleWebKit/537.36 '
+        #                         '(KHTML, like Gecko) Chrome/72.0.3626.121 '
+        #                         'Safari/537.36'}
 
         for item in wall_lamp_list:
             for link in item.find_all('a', href=True):
@@ -24,28 +35,49 @@ if __name__ == '__main__':
                 break
 
         for link in product_links:
-            r = requests.get(link, headers=headers)
-            soup = BeautifulSoup(r.content, 'html.parser')
-
-            name = soup.find('h1',
-                             class_='product_name__name m-0').text.strip()
-            price = soup.find('strong',
-                              class_='projector_prices__price').text.strip()
-            # specifications = soup.find('section', class_='dictionary
-            # col-12 mb-1 ' 'mb-sm-4').find_all("span",
-            # class_='dictionary__name_txt')
-
-            dicionary_name = soup.find_all('span',
-                                           class_='dictionary__name_txt')
-            dictionary_value = soup.find_all('span',
-                                             class_='dictionary__value_txt')
-
-            # for i in range(0, len(specifications)):
-            #     print(specifications[i].text.strip())
-            print()
-            wall_lamp = {
-                'Nazwa': name,
-                'Cena': price
-            }
+            print(f'Page link: {link}')
+            json_obj = json.dumps(product_to_dict(link), indent=4)
+            f.write(json_obj)
+            f.write(',\n')
 
     print(f'Number of wall lamps: {len(product_links)}')
+    f.write()
+    f.close()
+
+def product_to_dict(link: str) -> dict:
+    r = requests.get(link)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    name = soup.find('h1',
+                     class_='product_name__name m-0').text.strip()
+    price = soup.find('strong',
+                      class_='projector_prices__price').text.strip()
+
+    param_names = soup.find_all(class_='dictionary__name_txt')
+
+    param_values = soup.find_all(class_='dictionary__value_txt')
+
+    wall_lamp = {
+        'Nazwa': name,
+        'Cena': price,
+        'Parametry techniczne': {}
+    }
+
+    dict_list = []
+    for param_name, param_value in zip(param_names, param_values):
+        tmp_name = param_name.text.strip()
+        tmp_value = param_value.text.strip()
+        if tmp_name in ['Wykonanie', 'Kolor']:
+            wall_lamp['Parametry techniczne'][tmp_name] = re.split(
+                ', |/', tmp_value)
+        else:
+            wall_lamp['Parametry techniczne'][
+                param_name.text.strip()] = \
+                tmp_value
+
+    print(wall_lamp)
+    return wall_lamp
+
+
+if __name__ == '__main__':
+    main()
